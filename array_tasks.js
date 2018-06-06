493aaa0ad209cadf263b2d4a6af9ba715e6a4c09
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-06 16:04:02 +0100

    first test passing

--- array_tasks.js
+++ array_tasks.js
@@ -1,35 +1,36 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
-
-	// },
+	concat: function (arr1, arr2) {
+	var new_array =	arr1.concat(arr2);
+	return new_array;
+	},
 
 	// insertAt: function (arr, itemToAdd, index) {
-		
+
 	// },
 
 	// square: function (arr) {
-		
+
 	// },
 
 	// sum: function (arr) {
-		
+
 	// },
 
 	// findDuplicates: function (arr) {
-		
+
 	// },
 
 	// removeAndClone: function (arr, valueToRemove) {
-		
+
 	// },
 
 	// findIndexesOf: function (arr, itemToFind) {
-		
+
 	// },
 
 	// sumOfAllEvenNumbersSquared: function (arr) {
-		
+
 	// }
 
 }
